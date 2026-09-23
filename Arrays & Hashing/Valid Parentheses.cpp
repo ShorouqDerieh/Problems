ab1e0d7211abc00234fc2ff8315da4c59d988edc
ShorouqDerieh <s12217236@stu.najah.edu>
2026-09-23 22:29:30 +0300

    Valid Parentheses problem

---
+++ Arrays & Hashing/Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>p;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+        {
+            p.push(s[i]);
+        }
+        else if((!p.empty())&&((s[i]=='}'&&p.top()=='{')||(s[i]==')'&&p.top()=='(')
+        ||(s[i]==']'&&p.top()=='[')))
+        {
+            p.pop();
+        }
+        else{
+            return false;
+        }
+        }
+    if(p.empty())
+    {
+        return true;
+    }
+    else return false;
+    }
+};
